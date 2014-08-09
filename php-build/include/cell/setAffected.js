987052355e8093ca33c37c5f33b4b9aded36b7ae
Ikhsan Agustian <ikhsan017@gmail.com>
2014-08-09 17:51:14 +0700

    update setAffected() docblock

--- php-build/include/cell/setAffected.js
+++ php-build/include/cell/setAffected.js
@@ -1,6 +1,8 @@
 /**
- * mark cell as affected by other cell
+ * mark cell as affected by other cell, used to decide whether to
+ * process the cell or not when processing dependency tree
  */
 cell.prototype.setAffected = function(affected){
+    affected = typeof(affected) == 'undefined' ? true : false;
     this.affected = affected;
 };
