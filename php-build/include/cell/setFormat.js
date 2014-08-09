a392d871147f8ccda84495233a081e0bbd37f662
Ikhsan Agustian <ikhsan017@gmail.com>
2014-08-09 15:18:58 +0700

    update dockblock

--- php-build/include/cell/setFormat.js
+++ php-build/include/cell/setFormat.js
@@ -1,3 +1,7 @@
+/**
+ * set formatting rule to the cell
+ * @param {string} format       format rule to define formatting on rendered value
+ */
 cell.prototype.setFormat = function(format){
     this.format = format;
     if(false !== this.el){
--- php-build/include/cell/setFormula.js
+++ php-build/include/cell/setFormula.js
@@ -1,3 +1,7 @@
+/**
+ * set formula definition to the cell
+ * @param {string} formula       formula definition
+ */
 cell.prototype.setFormula = function(formula){
     this.formula = formula;
     if(false !== this.el){
