30e0d6bfa8cd23b59ad11b8f0ed804c68415aab4
Ikhsan Agustian <ikhsan017@gmail.com>
2014-08-10 18:36:57 +0700

    mark cell and it's dependant  as affected when changing the value

--- php-build/include/cell/setValue.js
+++ php-build/include/cell/setValue.js
@@ -1,7 +1,14 @@
+/**
+ * set cell value and sync it with the bound element, and trigger recalculation on all cell depend to it
+ * @param {mixed} value [description]
+ */
 cell.prototype.setValue = function(value){
+    var a;
     this.value          = value;
     this.floatValue     = ($.isNumeric(value)) ? parseFloat(value) : 0;
-    this.formattedValue = this.sheet.engine.formatter(
+    this.formattedValue = '';
+
+    this.sheet.engine.formatter(
                             this.floatValue,
                             (this.format) ? this.format : this.sheet.config.defaultFormat
                         );
@@ -15,5 +22,10 @@ cell.prototype.setValue = function(value){
         }else{
             this.el.html(this.value);
         }
+    }
+
+    this.setAffected(true);
+    for(a in this.dependant){
+        this.dependant[a].setAffected(true);
     }
 };
