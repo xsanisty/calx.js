105bd2f1f156006de2c549a97c1ec20a5e0d42a1
Ikhsan Agustian <ikhsan017@gmail.com>
2014-08-10 18:44:26 +0700

    only process affected cell, and mark itself and it dependant as not affected n already processed

--- php-build/include/cell/processDependency.js
+++ php-build/include/cell/processDependency.js
@@ -2,8 +2,16 @@ cell.prototype.processDependency = function(){
 
     /** process all dependencies first, then evaluate the formula */
     for (var a in this.dependencies){
-        this.dependencies[a].processDependency();
-    }
+        if(this.dependencies[a].isAffected()){
+            this.dependencies[a].processDependency();
+        }
+        this.dependencies[a].setAffected(false);
 
+    }
     this.evaluateFormula();
+    this.setAffected(false);
+
+    if(this.sheet.config.autoCalculate){
+        this.renderComputedValue();
+    }
 };
