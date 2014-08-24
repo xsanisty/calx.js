3086fd1d3c2ec5ff9052a2ec4baffbca07c2fcd3
Ikhsan Agustian <ikhsan017@gmail.com>
2014-08-25 06:02:28 +0700

    add docblock

--- php-build/include/cell/removeDependency.js
+++ php-build/include/cell/removeDependency.js
@@ -1,3 +1,8 @@
+/**
+ * remove key from the dependency list
+ * @param  {string} key [the dependency key, can be cellAddress, or #sheet>cellAddress]
+ * @return {void}
+ */
 cell.prototype.removeDependency = function(key){
     if(typeof(this.dependencies[key]) != 'undefined'){
         delete this.dependencies[key];
