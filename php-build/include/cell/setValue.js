e0bd40e767ae85c345b35706d3cae44348eae394
Ikhsan Agustian <ikhsan017@gmail.com>
2014-08-24 21:16:34 +0700

    fix setValue logic

--- php-build/include/cell/setValue.js
+++ php-build/include/cell/setValue.js
@@ -8,24 +8,11 @@ cell.prototype.setValue = function(value){
     this.floatValue     = ($.isNumeric(value)) ? parseFloat(value) : 0;
     this.formattedValue = '';
 
-    this.sheet.engine.formatter(
-                            this.floatValue,
-                            (this.format) ? this.format : this.sheet.config.defaultFormat
-                        );
-
-    if(false !== this.el){
-        var tagName     = this.el.prop('tagName').toLowerCase(),
-            isFormTag   = this.formTags.indexOf(tagName) > -1;
-
-        if(isFormTag){
-            this.el.val(this.value);
-        }else{
-            this.el.html(this.value);
-        }
-    }
-
     this.setAffected(true);
+
     for(a in this.dependant){
         this.dependant[a].setAffected(true);
     }
+
+    this.processDependant(true, true);
 };
