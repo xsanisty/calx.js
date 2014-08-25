7633d5a310c457c78dce44a0f9eaf260b525b703
Ikhsan Agustian <ikhsan017@gmail.com>
2014-08-26 06:28:11 +0700

    set value as float if it is numeric

--- php-build/include/cell/setValue.js
+++ php-build/include/cell/setValue.js
@@ -4,16 +4,14 @@
  * @param {bool}    render      render computed value of it's dependant or not
  */
 cell.prototype.setValue = function(value, render){
-    var a;
-    this.value          = value;
-    this.floatValue     = ($.isNumeric(value)) ? parseFloat(value) : 0;
+    this.value          = ($.isNumeric(value)) ? parseFloat(value) : value;
     this.formattedValue = '';
 
     render = (typeof(render) == 'undefined') ? true : render;
 
     this.setAffected(true);
 
-    for(a in this.dependant){
+    for(var a in this.dependant){
         this.dependant[a].setAffected(true);
     }
 
