58de32d5687cec6e002e8db58e28580b0511e7d6
Ikhsan Agustian <ikhsan017@gmail.com>
2014-08-26 06:05:05 +0700

    add override option

--- php-build/include/calx/registerFunction.js
+++ php-build/include/calx/registerFunction.js
@@ -3,6 +3,15 @@
  * @param  {[type]} argument [description]
  * @return {[type]}          [description]
  */
-registerFunction : function (funcName, funcDefinition) {
+registerFunction : function (funcName, funcDefinition, override) {
+    override = (typeof(override) == 'undefined') ? false : override;
+
+    if(override){
+        for(var a in formula){
+            if(typeof(formula[a][funcName]) != 'undefined'){
+                delete(formula[a][funcName]);
+            }
+        }
+    }
     formula.user_defined[funcName] = funcDefinition;
 }
