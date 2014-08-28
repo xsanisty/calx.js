cef024027e1c8da2dfe48fa6a0db0ab8229a32b1
Ikhsan Agustian <ikhsan017@gmail.com>
2014-08-29 06:53:04 +0700

    add SERVER() function

--- php-build/include/formula/custom.js
+++ php-build/include/formula/custom.js
@@ -1,3 +1,31 @@
 custom : {
+    SERVER : function(){
+        if(this.config.ajaxUrl == null){
+            return '#NAME?';
+        }
 
+
+        var result,
+            funcName = arguments[0],
+            params = {};
+
+        for (var a = 1; a < arguments.length; a++){
+            params['params['+a+']'] = arguments[a];
+        }
+
+        $.ajax({
+            url: this.config.ajaxUrl,
+            method: this.config.ajaxMethod,
+            data: params,
+            async: false,
+            success: function(response){
+                result = response;
+            },
+            error: function(response){
+                result = '#NAME?';
+            }
+        });
+
+        return result;
+    }
 }
