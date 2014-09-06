dac1cb721293e8bf43cb6ca6247a7e7b1fb7f2ea
Ikhsan Agustian <ikhsan017@gmail.com>
2014-09-07 06:28:07 +0700

    fix sheet reset function & update build

--- jquery-calx-2.0.0.js
+++ jquery-calx-2.0.0.js
@@ -8242,11 +8242,20 @@ sheet.prototype.applyChange = function(){
 };sheet.prototype.refresh = function(){
 
 };sheet.prototype.reset = function(){
+    var forms;
+
     if(this.el.prop('tagName').toLowerCase() == 'form'){
-        this.el.reset();
+        forms = this.el;
+    }else{
+        forms = this.el.find('form');
     }
+
+    forms.each(function(){
+        this.reset();
+    });
+
     for(var a in this.cells){
-        this.cells[a].resyncVaue();
+        this.cells[a].resyncValue();
     }
 };sheet.prototype.attachEvent = function(){
 
--- php-build/include/sheet/reset.js
+++ php-build/include/sheet/reset.js
@@ -1,8 +1,17 @@
 sheet.prototype.reset = function(){
+    var forms;
+
     if(this.el.prop('tagName').toLowerCase() == 'form'){
-        this.el.reset();
+        forms = this.el;
+    }else{
+        forms = this.el.find('form');
     }
+
+    forms.each(function(){
+        this.reset();
+    });
+
     for(var a in this.cells){
-        this.cells[a].resyncVaue();
+        this.cells[a].resyncValue();
     }
 };
