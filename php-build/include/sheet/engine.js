21e90c5f8339be57192e343dd6a4189092d8075a
Ikhsan Agustian <ikhsan017@gmail.com>
2014-09-10 11:19:23 +0700

    remove engine from sheet

--- php-build/include/sheet/engine.js
+++
@@ -1,8 +0,0 @@
-    /**
-     * Sheet prototype engine, contain formatting engine, formula
-     * @type {Object}
-     */
-    sheet.prototype.engine = {
-        <?php require 'include/sheet/engine/formatter.js' ?>,
-        <?php require 'include/sheet/engine/bignumber.js' ?>
-    };
