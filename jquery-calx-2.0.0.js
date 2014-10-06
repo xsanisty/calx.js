b05205e1f16294029b3a08b5d04f5c2bcdadaf0c
Ikhsan Agustian <ikhsan017@gmail.com>
2014-10-06 20:45:34 +0700

    update build

--- jquery-calx-2.0.0.js
+++ jquery-calx-2.0.0.js
@@ -8175,6 +8175,7 @@ cell.prototype.setProcessed = function(processed){
     this.processed = (typeof(processed) == 'undefined') ? true : processed;
 
     //console.log('cell[#'+this.sheet.elementId+'!'+this.address+'] : mark as processed ['+processed+']');
+    return this;
 }/**
  * [isProcessed description]
  * @return {Boolean} [description]
