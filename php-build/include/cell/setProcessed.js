cb2c371e0ccb067a49aa9ffd699d5bd0b4877002
Ikhsan Agustian <ikhsan017@gmail.com>
2014-10-06 20:43:19 +0700

    return this on setProcessed

--- php-build/include/cell/setProcessed.js
+++ php-build/include/cell/setProcessed.js
@@ -6,4 +6,5 @@ cell.prototype.setProcessed = function(processed){
     this.processed = (typeof(processed) == 'undefined') ? true : processed;
 
     //console.log('cell[#'+this.sheet.elementId+'!'+this.address+'] : mark as processed ['+processed+']');
+    return this;
 }
