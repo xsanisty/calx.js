267a19e13855bcc662f7d4c58f127bad284f07dd
Ikhsan Agustian <ikhsan017@gmail.com>
2014-10-18 20:29:12 +0700

    re-implement getFormattedValue

--- php-build/include/cell/getFormattedValue.js
+++ php-build/include/cell/getFormattedValue.js
@@ -3,5 +3,18 @@
  * @return {string}     the formatted value
  */
 cell.fx.getFormattedValue = function(){
-    return this.formattedValue;
+    var originalVal = (this.formula) ? this.computedValue : this.value,
+        formattedVal= (
+            this.format != ''
+            && typeof(numeral) != 'undefined'
+            && originalVal !== ''
+            && originalVal !== false
+            && originalVal !== null
+            && data.ERROR.indexOf(originalVal) == -1
+            && $.isNumeric(originalVal)
+        )
+        ? numeral(originalVal).format(this.format)
+        : originalVal;
+
+    return formattedVal;
 };
