e21469ae8fa4afe926143be70ad4b39eb4406a2d
Ikhsan Agustian <ikhsan017@gmail.com>
2015-04-14 08:23:13 +0700

    add new config key: data , to instantiate the sheet data, format, and formula

--- php-build/include/sheet/registerCell.js
+++ php-build/include/sheet/registerCell.js
@@ -4,7 +4,25 @@
  * @return {void}
  */
 sheet.fx.registerCell = function(cell){
-    this.cells[cell.getAddress()] = cell;
+    var address = cell.getAddress()
+    this.cells[address] = cell;
+
+    if(typeof this.config.data[address] != 'undefined'){
+        var cellConfig = this.config.data[address];
+
+        if(typeof cellConfig.value != 'undefined'){
+            cell.setValue(cellConfig.value);
+            cell.renderComputedValue();
+        }
+
+        if(typeof cellConfig.format != 'undefined'){
+            cell.setFormat(cellConfig.format);
+        }
+
+        if(typeof cellConfig.formula != 'undefined'){
+            cell.setFormula(cellConfig.formula);
+        }
+    }
 
     if(this.affectedCell.indexOf(cell.getAddress()) == -1){
         this.affectedCell.push(cell.getAddress());
