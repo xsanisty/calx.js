551e4ff4e8da7ce066aa402517bd41ae5f24c961
Ikhsan Agustian <ikhsan017@gmail.com>
2015-12-15 07:33:15 +0700

    add docblock to sheet variable

--- php-build/include/sheet.js
+++ php-build/include/sheet.js
@@ -6,19 +6,46 @@
  * @return {void}
  */
 function sheet(identifier, element, config){
+    /** @type {string} The sheet unique identifier */
     this.identifier   = identifier;
+
+    /** @type {object} jQuery dom element */
     this.el           = $(element);
+
+    /** @type {string} Default language setting */
     this.lang         = 'en';
+
+    /** @type {object} Cells registry containing all available cell */
     this.cells        = {};
+
+    /** @type {object} Variable registry, containing all defined variable */
     this.variables    = {};
+
+    /** @type {object} Sheet configuration object */
     this.config       = $.extend({}, defaultConfig, config);
+
+    /** @type {number} Cell counter, count total cell in the sheet */
     this.counter      = 1;
+
+    /** @type {boolean} Indicate if the sheet has relation with other sheet */
     this.relatedSheet = false;
+
+    /** @type {string} The id of the element where the sheet is initialized and bound */
     this.elementId    = this.el.attr('id');
+
+    /** @type {object} Registry containing sheets that depend on this sheet */
     this.dependant    = {};
+
+    /** @type {object} Registry containing sheets that this sheet depend on*/
     this.dependencies = {};
+
+    /** @type {boolean} Indicate the current sheet calculation is done */
     this.calculated   = false;
+
+    /** @type {boolean} Indicate the current sheet calculation is in progress */
     this.calculating  = false;
+
+    /** @type {object} Current cell object being calculated */
     this.activeCell   = null;
     this.totalCell    = 0;
     this.affectedCell = [];
