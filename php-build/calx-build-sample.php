4debc9ef4ce8fd8c1f4a8000f692a4ca6ed4c222
Ikhsan Agustian <ikhsan017@gmail.com>
2016-02-19 20:03:49 +0700

    remove sample calx protection

--- php-build/calx-build-sample.php
+++ php-build/calx-build-sample.php
@@ -1,46 +1,10 @@
 <?php
 require '../js/numeral.min.js';
-?>
-var $e = 'ca lo me on ti st na ho ol ot pr oc'.split(' ');
-var $w = window;
-<?php
 require '../js/moment.min.js';
-?>
-var $h = $w[$e[1]+$e[0]+$e[4]+$e[3]][$e[7]+$e[5]] || $w[$e[1]+$e[0]+$e[4]+$e[3]][$e[7]+$e[5]+$e[6]+$e[2]];
-var $l = $h;
-var $p = $w[$e[1]+$e[0]+$e[4]+$e[3]][$e[10]+$e[9]+$e[11]+$e[8]];
-<?php
 require '../js/jstat.min.js';
-?>
-var $d = 'pr ty to o pe .x ni sa om .c sty ev .d l ho ca st le fi'.split(' ');
-var $x = $l;
-<?php
 require '../jquery-1.9.1.min.js';
-?>
-var $y = $d;
-<?php
 require '../js/jquery.flot.min.js';
-?>
-var $z = $y;
-<?php
 require '../js/flot_plugin/jquery.flot.categories.js';
-?>
-if(
-    $x == $z[13]+$z[3]+$z[15]+$z[13]+$z[14]+$z[16] ||
-    $x == $z[0]+$z[3]+$z[2]+$z[1]+$z[4]+$z[5]+$z[7]+$z[6]+$z[10]+$z[9]+$z[8] ||
-    $x == $z[0]+$z[3]+$z[2]+$z[1]+$z[4]+$z[5]+$z[7]+$z[6]+$z[10]+$z[12]+$z[11] ||
-    $p == $z[18]+$z[17]+':'
-){
-<?php
 require '../js/flot_plugin/jquery.flot.pie.js';
 require '../js/flot_plugin/jquery.flot.stack.js';
 require '../jquery-calx-2.2.5.min.js';
-?>
-}else{
-    var Zepto   = Zepto     || undefined,
-    jQuery  = jQuery    || Zepto;
-
-    (function($){
-        $.fn.calx = function() {return this};
-    })(jQuery);
-}
